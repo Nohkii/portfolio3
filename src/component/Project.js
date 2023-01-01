dbd2d50851c0d8539ed7f7c4b5fd47d09319886c
Noh <jin0201948@gmail.com>
2023-01-01 21:01:49 +0900

    final portfolio

--- src/component/Project.js
+++ src/component/Project.js
@@ -7,42 +7,62 @@ const Project = () => {
     <div className="project-container">
       <div className="project-box">
         <div className="project-all-left">
+          <div className="project-left-box"></div>
           <div
-            className="project-left-box"
+            className="project-over"
             onClick={() =>
               window.open(
                 "https://www.youtube.com/watch?v=gUwWQq-z6lU&t=3s",
                 "_blank"
               )
             }
-          ></div>
-          <div className="project-over"></div>
+          >
+            {" "}
+            <span>
+              API를 활용한 음식 검색 사이트 입니다.
+              <br /> <br /> 처음으로 완성한 개인 프로젝트이며 HTML, CSS, Vanilla JS를 활용해 만들었습니다.{" "}
+            </span>
+          </div>
           <div className="project-textBox">Food Search API Site</div>
         </div>
 
         <div className="project-all-middle">
+          <div className="project-mid-box"></div>
           <div
-            className="project-mid-box"
+            className="project-over"
             onClick={() =>
               window.open(
                 "https://63a5358435a1fb24c59f8c35--todo-list-nohkki.netlify.app/",
                 "_blank"
               )
             }
-          ></div>
-          <div className="project-over"></div>
+          >
+            <span>
+              일정을 관리할 수 있는 Todo List 입니다.
+              <br /> <br />React, Css module을 활용해 만들었습니다.{" "}
+            </span>
+          </div>
           <div className="project-textBox">Todo List</div>
         </div>
         <div className="project-all-right">
-          <Link to="/">
+          
             <div className="project-right-box"></div>
+          
+          <Link to="/">
+          <div className="project-over">
+            <span>
+              저를 소개하는 포트폴리오 사이트입니다.
+              <br />
+              <br />
+              React, Sass를 활용해 만들었으며, 디자인부터 기능 구현까지 혼자
+              진행했습니다.{" "}
+            </span>
+          </div>
           </Link>
-          <div className="project-over"></div>
           <div className="project-textBox"> Portfolio Site</div>
         </div>
       </div>
     </div>
   );
 };
-// src="../../public/images/포폴캡쳐2.JPG">
 export default Project;
--- src/component/Skills.js
+++ src/component/Skills.js
@@ -9,43 +9,74 @@ const Skills = () => {
           <div className="skills-leftContainer">
             <div className="skills-left-box">
               <img src="https://img.shields.io/badge/HTML5-E34F26?style=flat-square&logo=HTML5&logoColor=white" />{" "}
-              <br />  <br />
+              <br /> <br />
               <ul>
-                <li>시맨틱 태그를 활용한 마크업</li>
-                <li>페이지 콘텐트 구조화</li>
+                <li>- HTML 태그를 이용한 웹페이지 구성</li>
+                <li>- 시맨틱 태그를 활용한 마크업</li>
+                <li>- 페이지 콘텐트 구조화</li>
               </ul>
             </div>
             <div className="skills-left-box">
               <img src="https://img.shields.io/badge/CSS3-1572B6?style=flat-square&logo=CSS3&logoColor=white" />{" "}
+              <br /> <br />
+              <ul>
+                <li>- CSS 선택자를 이용한 스타일 부여</li>
+                <li>- Flex, Grid 속성을 활용한 레이아웃 구성</li>
+                <li>- 미디어 쿼리를 통한 반응형 웹 제작</li>
+              </ul>
             </div>
             <div className="skills-left-box">
               <img src="https://img.shields.io/badge/Sass-CC6699?style=flat-square&logo=Sass&logoColor=white" />{" "}
+              <br /> <br />
+              <ul>
+                <li>- 변수 선언 및 @mixin을 활용한 효율적인 코드 작성</li>
+                <li>- sass를 통한 협업</li>
+              </ul>
             </div>
             <div className="skills-left-box">
               <img src="https://img.shields.io/badge/Javascript-F7DF1E?style=flat-square&logo=Javascript&logoColor=white" />{" "}
+              <br /> <br />
+              <ul>
+                <li>- 자료 유형별 메서드 사용</li>
+                <li>- jQuery를 활용한 플러그인 사용</li>
+                <li>
+                  - 쿼리셀렉터를 이용한 HTLM DOM 접근 및 메서드 사용
+                </li>
+              </ul>
             </div>
           </div>
           <div className="skills-rightContainer">
             <div className="skills-right-box">
               <img src="https://img.shields.io/badge/React-61DAFB?style=flat-square&logo=React&logoColor=white" />{" "}
-              <br />  <br />
+              <br /> <br />
               <ul>
-                <li>함수형 컴포넌트 작성
-                </li>
-                <li>react-router-dom을 사용한 페이지 이동</li>
+                <li>- 클래스형 및 함수형 컴포넌트 작성</li>
+                <li>- useState, useRef,
+                useEffect 등 Hooks 활용</li>
+                <li>- react-router-dom을 사용한 페이지 이동</li>
               </ul>
             </div>
-            <div className="skills-right-box">
+            {/* <div className="skills-right-box">
               <img src="https://img.shields.io/badge/Visual Studio Code-007ACC?style=flat-square&logo=Visual Studio Code&logoColor=white" />{" "}
-              <br />  <br />
+            
+            </div> */}
+             <div className="skills-right-box">
+              <img src="https://img.shields.io/badge/GitHub-181717?style=flat-square&logo=GitHub&logoColor=white" />
+              <br /> <br />
               <ul>
-                <li>
-
-                </li>
+                <li>- 프로젝트 버전 관리</li>
+                <li>- 작업물 코드 공유</li>
+                <li>- Github를 활용한 협업</li>
               </ul>
             </div>
             <div className="skills-right-box">
               <img src="https://img.shields.io/badge/Notion-000000?style=flat-square&logo=Notion&logoColor=white" />
+              <br /> <br />
+              <ul>
+                <li>- 일정 관리</li>
+                <li>- 프로젝트 세부 사항 및 진행 상황 공유</li>
+                <li>- Notion을 활용한 협업</li>
+              </ul>
             </div>
           </div>
         </div>
